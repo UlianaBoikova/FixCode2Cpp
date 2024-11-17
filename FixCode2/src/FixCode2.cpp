217620ac54b996a1533d2069d27f7b4e71f04d0f
ulianaboikova <ulyanaboykova49@gmail.com>
2024-11-16 22:30:56 -0800

    Initial Commit

---
+++ FixCode2/src/FixCode2.cpp
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : FixCode2.cpp
+// Author      : Uliana Boikova
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include<iostream>
+
+using namespace std;
+
+
+int main() {
+    double myMoney = 1000.50;
+
+    cout << "Please be sure to correct all syntax errors in this program" << endl;
+    cout << "I have corrected all errors for this program." << endl;
+
+    cout << "The total amount of money available is = " << myMoney << endl;
+
+    return 0;
+}
